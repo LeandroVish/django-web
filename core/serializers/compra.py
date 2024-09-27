2fdf99ba95c856f93dcac3ebd793e866750c60ce
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-09-27 08:49:11 -0300

    27 - Criação de um endpoint para criar novas compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -17,6 +17,25 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ("id", "usuario", "status", "total", "itens")
-        usuario = CharField(source="user.email", read_only=True)  # inclua essa linha
-        status = CharField(source="get_status_display", read_only=True)  # inclua essa linha
+        usuario = CharField(source="user.email", read_only=True) 
+        status = CharField(source="get_status_display", read_only=True) 
         itens = ItensCompraSerializer(many=True, read_only=True)
+
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True)
+
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
