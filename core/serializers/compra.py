b29abbf770d51d94bb7d81d2ad4f07de68a0b9cd
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-10-18 08:36:53 -0300

    33 - Acrescentando a data da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import (
     CharField,
     CurrentUserDefault,
+    DateTimeField,
     HiddenField,
     ModelSerializer,
     SerializerMethodField,
@@ -42,11 +43,14 @@ class ListarCompraSerializer(ModelSerializer):
 
 class CompraSerializer(ModelSerializer):
     class Meta:
-        model = Compra
-        fields = ("id", "usuario", "status", "total", "itens")
         usuario = CharField(source="user.email", read_only=True)
         status = CharField(source="get_status_display", read_only=True)
+        data = DateTimeField(read_only=True)
         itens = ItensCompraSerializer(many=True, read_only=True)
+
+        class Meta:
+            model = Compra
+            fields = ("id", "usuario", "status", "total", "data", "itens")
 
 
 class CriarEditarItensCompraSerializer(ModelSerializer):
